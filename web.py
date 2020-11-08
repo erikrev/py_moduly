6e47e958b7e04bac39e2702bb4763959267522e5
Erik Revaj <erik.revaj3@gmail.com>
2020-11-08 22:34:14 +0100

    open browser

--- web.py
+++ web.py
@@ -1,3 +1,8 @@
 import webbrowser
+#
+# webbrowser.open("https://www.python.org/", new=2)
+# webbrowser.open_new("https://www.python.org/")
+# help(webbrowser)
 
-webbrowser.open("https://www.python.org/")
+chrome = webbrowser.get(using="chromium")
+chrome.open_new("https://www.python.org/")
