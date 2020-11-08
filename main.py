55fe02118bc192b653a94bfab77ec8ec7def112d
Erik Revaj <erik.revaj3@gmail.com>
2020-11-08 21:40:31 +0100

    added webbrowser file

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 # print(dir(__builtins__))
 # for m in dir(__builtins__):
 #     print(m)
-import shelve
-print(dir(shelve))
-
-for obj in dir(shelve.Shelf):
-    if obj[0] != "_":
-        print(obj)
-
+# import shelve
+# print(dir(shelve))
+#
+# for obj in dir(shelve.Shelf):
+#     if obj[0] != "_":
+#         print(obj)
+#
+#
+# help(shelve)
 
